04a2866ca775c278b4408e1189cc6134b57378d5
Ridwan Rahman <ridwan@clipsalsolar.com>
2022-06-02 23:36:06 +1000

    Added models

--- data_loader/admin.py
+++ data_loader/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company, Person, Fruit, Vegetable, PersonFriend
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Person)
+admin.site.register(PersonFriend)
+admin.site.register(Fruit)
+admin.site.register(Vegetable)
