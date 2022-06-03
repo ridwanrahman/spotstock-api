b21025ee2edfff4b88f896406ea6f8eb99c0e0b2
Ridwan Rahman <ridwan@clipsalsolar.com>
2022-06-03 19:16:07 +1000

    Added solution to first question

--- api/urls.py
+++ api/urls.py
@@ -9,8 +9,12 @@ router.register(r'person', views.PersonViewSet)
 router.register(r'personfriend', views.PersonFriendViewSet)
 router.register(r'fruit', views.FruitViewSet)
 router.register(r'vegetable', views.VegetableViewSet)
+# router.register(r'question_one', views.QuestionOne)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('q1/<int:company_index>', views.QuestionOne.as_view()),
+    path('q2', views.QuestionTwo.as_view()),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+urlpatterns += router.urls
--- api/views.py
+++ api/views.py
@@ -5,6 +5,12 @@ from rest_framework import viewsets
 from api.serializers import CompanySerializer, PersonSerializer, PersonFriendSerializer, FruitSerializer, VegetableSerializer
 from data_loader.models import Company, Person, PersonFriend, Fruit, Vegetable
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# from django.http.HttpRequest import Request
+
+from rest_framework.decorators import api_view
+
 
 class CompanyViewSet(viewsets.ModelViewSet):
     """
@@ -44,3 +50,36 @@ class VegetableViewSet(viewsets.ModelViewSet):
     """
     queryset = Vegetable.objects.all()
     serializer_class = VegetableSerializer
+
+
+# @api_view(['GET'])
+# def question_one(request):
+#     return Response({
+#         'status': 200,
+#         'message': 'success'
+#     })
+class QuestionOne(APIView):
+    def get(self, request, company_index):
+        """
+        Return a list of all users.
+        """
+        serializer_context = {
+            'request': request,
+        }
+
+        id = company_index or request.query_params.get('company_index')
+        people_working_in_company = Person.objects.filter(company_id__index=id)
+        if not people_working_in_company:
+            return Response({
+                'response': "Nobody working in this company"
+            })
+        serializer = PersonSerializer(people_working_in_company, many=True, context=serializer_context)
+        return Response(serializer.data)
+
+class QuestionTwo(APIView):
+    def get(self, request):
+        person1 = self.request.query_params.get('person1')
+        person2 = self.request.query_params.get('person1')
+
+        print(person1)
+        print(person2)
