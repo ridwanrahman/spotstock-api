df9d81dac80309c9cede3458c3273d8556b6cba1
Ridwan Rahman <ridwan@clipsalsolar.com>
2022-06-06 01:46:28 +1000

    Fixed common people test

--- api/tests.py
+++ api/tests.py
@@ -191,88 +191,76 @@ class TestCommonPeople(TestCase):
         # person index 0 & 2 should return person index 3 who has brown eye
         records = [
             {
+                "_id": "595eeb9b96d80a5bc7afb106",
                 "index": 0,
                 "guid": "5e71dc5d-61c0-4f3b-8b92-d77310c7fa43",
                 "has_died": True,
+                "balance": "$2,418.59",
+                "picture": "http://placehold.it/32x32",
                 "age": 61,
                 "eyeColor": "blue",
-                "name": "TEST USER_1",
-                "company_id": company.index,
+                "name": "Carmella Lambert",
+                "gender": "female",
+                "company_id": 58,
                 "email": "carmellalambert@earthmark.com",
                 "phone": "+1 (910) 567-3630",
                 "address": "628 Sumner Place, Sperryville, American Samoa, 9819",
+                "about": "Non duis dolore ad enim. Est id reprehenderit cupidatat tempor excepteur. Cupidatat labore incididunt nostrud exercitation ullamco reprehenderit dolor eiusmod sit exercitation est. Voluptate consectetur est fugiat magna do laborum sit officia aliqua magna sunt. Culpa labore dolore reprehenderit sunt qui tempor minim sint tempor in ex. Ipsum aliquip ex cillum voluptate culpa qui ullamco exercitation tempor do do non ea sit. Occaecat laboris id occaecat incididunt non cupidatat sit et aliquip.\r\n",
+                "registered": "2016-07-13T12:29:07 -10:00",
+                "tags": [
+                    "id",
+                    "quis",
+                    "ullamco",
+                    "consequat",
+                    "laborum",
+                    "sint",
+                    "velit"
+                ],
                 "friends": [
+                    {
+                        "index": 0
+                    },
                     {
                         "index": 1
                     },
                     {
                         "index": 2
-                    },
-                    {
-                        "index": 3
                     }
                 ],
-                "favouriteFood": [],
+                "greeting": "Hello, Carmella Lambert! You have 6 unread messages.",
+                "favouriteFood": [
+                    "orange",
+                    "apple",
+                    "banana",
+                    "strawberry"
+                ]
             },
             {
+                "_id": "595eeb9b1e0d8942524c98ad",
                 "index": 1,
-                "guid": "5e71dc5d-61c0-4f3b-8b92-d77310fa43",
-                "has_died": True,
-                "age": 61,
-                "eyeColor": "brown",
-                "name": "TEST USER_2",
-                "company_id": company.index,
-                "email": "carmellalambert@earthmark.com",
-                "phone": "+1 (910) 567-3630",
-                "address": "628 Sumner Place, Sperryville, American Samoa, 9819",
-                "friends": [
-                    {
-                        "index": 0
-                    },
-                    {
-                        "index": 2
-                    },
-                    {
-                        "index": 3
-                    }
-                ],
-                "favouriteFood": [],
-            },
-            {
-                "index": 2,
-                "guid": "5e71dc5d-61c0-4f3b",
-                "has_died": True,
-                "age": 61,
-                "eyeColor": "blue",
-                "name": "TEST USER_3",
-                "company_id": company.index,
-                "email": "carmellalambert@earthmark.com",
-                "phone": "+1 (910) 567-3630",
-                "address": "628 Sumner Place, Sperryville, American Samoa, 9819",
-                "friends": [
-                    {
-                        "index": 0
-                    },
-                    {
-                        "index": 1
-                    },
-                    {
-                        "index": 3
-                    }
-                ],
-                "favouriteFood": [],
-            },
-            {
-                "index": 3,
-                "guid": "5e71dc5d--4f3b-8b92-d77310fa43",
+                "guid": "b057bb65-e335-450e-b6d2-d4cc859ff6cc",
                 "has_died": False,
-                "age": 61,
+                "balance": "$1,562.58",
+                "picture": "http://placehold.it/32x32",
+                "age": 60,
                 "eyeColor": "brown",
-                "name": "TEST USER_4",
-                "company_id": company.index,
-                "email": "carmellalambert@earthmark.com",
-                "phone": "+1 (910) 567-3630",
-                "address": "628 Sumner Place, Sperryville, American Samoa, 9819",
+                "name": "Decker Mckenzie",
+                "gender": "male",
+                "company_id": 98,
+                "email": "deckermckenzie@earthmark.com",
+                "phone": "+1 (893) 587-3311",
+                "address": "492 Stockton Street, Lawrence, Guam, 4854",
+                "about": "Consectetur aute consectetur dolor aliquip dolor sit id. Sint consequat anim occaecat ad mollit aliquip ut aute eu culpa mollit qui proident eu. Consectetur ea et sit exercitation aliquip officia ea aute exercitation nulla qui sunt labore. Enim veniam labore do irure laborum aute exercitation consectetur. Voluptate adipisicing velit sunt consectetur id sint adipisicing elit elit pariatur officia amet officia et.\r\n",
+                "registered": "2017-06-25T10:03:49 -10:00",
+                "tags": [
+                    "veniam",
+                    "irure",
+                    "mollit",
+                    "sunt",
+                    "amet",
+                    "fugiat",
+                    "ex"
+                ],
                 "friends": [
                     {
                         "index": 0
@@ -284,7 +272,58 @@ class TestCommonPeople(TestCase):
                         "index": 2
                     }
                 ],
-                "favouriteFood": [],
+                "greeting": "Hello, Decker Mckenzie! You have 2 unread messages.",
+                "favouriteFood": [
+                    "cucumber",
+                    "beetroot",
+                    "carrot",
+                    "celery"
+                ]
+            },
+            {
+                "_id": "595eeb9bb3821d9982ea44f9",
+                "index": 2,
+                "guid": "49c04b8d-0a96-4319-b310-d6aa8269adca",
+                "has_died": False,
+                "balance": "$2,119.44",
+                "picture": "http://placehold.it/32x32",
+                "age": 54,
+                "eyeColor": "blue",
+                "name": "Bonnie Bass",
+                "gender": "female",
+                "company_id": 59,
+                "email": "bonniebass@earthmark.com",
+                "phone": "+1 (823) 428-3710",
+                "address": "455 Dictum Court, Nadine, Mississippi, 6499",
+                "about": "Non voluptate reprehenderit ad elit veniam nulla ut ea ex. Excepteur exercitation aliquip Lorem nisi duis. Ex cillum commodo labore sint non velit aliquip cupidatat sint. Consequat est sint do in eiusmod minim exercitation do consectetur incididunt culpa deserunt. Labore veniam elit duis minim magna et laboris sit labore eu velit cupidatat cillum cillum.\r\n",
+                "registered": "2017-06-08T04:23:18 -10:00",
+                "tags": [
+                    "quis",
+                    "sunt",
+                    "sit",
+                    "aliquip",
+                    "pariatur",
+                    "quis",
+                    "nulla"
+                ],
+                "friends": [
+                    {
+                        "index": 0
+                    },
+                    {
+                        "index": 1
+                    },
+                    {
+                        "index": 2
+                    }
+                ],
+                "greeting": "Hello, Bonnie Bass! You have 10 unread messages.",
+                "favouriteFood": [
+                    "orange",
+                    "beetroot",
+                    "banana",
+                    "strawberry"
+                ]
             }
         ]
 
@@ -296,7 +335,7 @@ class TestCommonPeople(TestCase):
         test_common_peopple = CommonPeople()
         response = test_common_peopple.get_response(0, 2)
         common_friends = response[2]['common_friends']
-        assert (common_friends[0] == 'TEST USER_4')
+        assert (common_friends[0] == 'Decker Mckenzie')
 
     def test_common_people_same_query_person(self):
         """
